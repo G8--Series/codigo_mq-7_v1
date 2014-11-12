a4410f5875835f18a4a961df25840ef70900adfc
Alvaro <amelendez9901@gmail.com>
2014-11-11 21:20:12 -0600

    Update codigo_mq-7_v1.ino

--- codigo_mq-7_v1.ino
+++ codigo_mq-7_v1.ino
@@ -1,5 +1,5 @@
 // to the pins used:
-const int triggerPin = 12;
+const int triggerPin = 7;
 const int sensorPin = 5;
 int val = 0;
 int cont = 0;
@@ -11,15 +11,16 @@ void setup() {
 
 void loop() {
   // 60s high voltage 5.0v
-  Serial.println( "Heating Sensor" );
-  analogWrite(triggerPin, 255);  // triger ping y volataje 255 = 5v
-  delay(60000);
+  // Serial.println( "Heating Sensor" );
+  //analogWrite(triggerPin, 255);  // triger ping y volataje 255 = 5v
+  //delay(60000);
   // 90s low voltage 1.4v
-  analogWrite(triggerPin, 67);// triger ping y volataje 67 = 1.4v
+  //analogWrite(triggerPin, 67);// triger ping y volataje 67 = 1.4v
   val = analogRead( sensorPin );  
   
-  for ( cont = 0; cont < 90; cont++ )
+  for ( cont = 0; cont < 10; cont++ )
   {
+    
     Serial.print("Cantidad de monoxido: ");
     Serial.println(val);
     delay(1000);
