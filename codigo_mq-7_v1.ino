a59dcea4262d6ab29e3e0957275bc805010a801b
Alvaro <amelendez9901@gmail.com>
2014-11-16 22:33:10 -0600

    Update codigo_mq-7_v1.ino

--- codigo_mq-7_v1.ino
+++ codigo_mq-7_v1.ino
@@ -3,6 +3,7 @@ const int triggerPin = 7;
 const int sensorPin = 5;
 int val = 0;
 int cont = 0;
+int cont2 = 0;
 
 void setup() {
   pinMode(triggerPin, OUTPUT); 
@@ -11,7 +12,7 @@ void setup() {
 
 void loop() {
   // 60s high voltage 5.0v
-  // Serial.println( "Heating Sensor" );
+ // Serial.println( "Heating Sensor" );
   //analogWrite(triggerPin, 255);  // triger ping y volataje 255 = 5v
   //delay(60000);
   // 90s low voltage 1.4v
@@ -24,6 +25,7 @@ void loop() {
     Serial.print("Cantidad de monoxido: ");
     Serial.println(val);
     delay(1000);
+    EEPROM.write(cont2,val); //incluir librerias EEPROM
   }
   
 }
